1e836a04e7c5e61168e91cade0b9d6c9da889d8c
Darragh Hayes <dara.hayes.1995@gmail.com>
2016-04-25 18:37:25 +0100

    history and sync tokens

--- client.js
+++ client.js
@@ -6,15 +6,13 @@ const Config = require('./config')
 Seneca.client({port: Config.service.port});
 console.log('Client')
 
-let workouts = {
-    data: 'this is the users workouts'
-} 
+let workouts = ['hello'] 
 
 Seneca.act({role: 'calendar', cmd: 'save_history', workouts: workouts, userId: '12345'}, (err, results) => {
   console.log(err);
   // console.log(JSON.stringify(results, null, 2))
   console.log(JSON.stringify(results.sync_token, null, 2))
-  results.history.workouts.data = 'New Updated workouts';
+  results.history.workouts[0] = 'New Updated workouts';
   console.log(JSON.stringify(results.history, null, 2))
   Seneca.act({role: 'calendar', cmd: 'save_history', workouts: results.history.workouts, userId: '12345'}, (err, results) => {
     console.log(err);
